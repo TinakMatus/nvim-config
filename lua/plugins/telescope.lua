c3ce0f32b1b20f4327142fc1e7446a35ee8905a5
tinakmatus <matustinak1@gmail.com>
2025-12-25 22:08:27 +0100

    even better telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,18 +9,17 @@ return {
     config = function()
       require('telescope').setup {
         pickers = {
-          find_files = {
-            theme = "ivy"
+          find_files = {theme = "ivy"},
+            git_files = { theme = "ivy" }
           }
         }
-      }
 
       local builtin = require('telescope.builtin')
 
       -- Keymaps from the video:
       vim.keymap.set("n", "<space>fh", builtin.help_tags)
       vim.keymap.set("n", "<space>fd", builtin.find_files)
-      
+      vim.keymap.set("n", "<space>gf", builtin.git_files)
       -- Special shortcut to find files in your Neovim config folder
       vim.keymap.set("n", "<space>en", function()
         builtin.find_files {
