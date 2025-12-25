463e7e078db3fccd07cab39ce495f33408825d09
tinakmatus <matustinak1@gmail.com>
2025-12-25 21:58:23 +0100

    better telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,32 @@
-return{
-      {
-    'nvim-telescope/telescope.nvim', 
-     tag = 'v0.2.0',
-     dependencies = { 'nvim-lua/plenary.nvim' },
-    local builtin = require('telescope.builtin'),
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' }),
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' }),
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' }),
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' }),
-    }
+return {
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
+    config = function()
+      require('telescope').setup {
+        pickers = {
+          find_files = {
+            theme = "ivy"
+          }
+        }
+      }
 
+      local builtin = require('telescope.builtin')
+
+      -- Keymaps from the video:
+      vim.keymap.set("n", "<space>fh", builtin.help_tags)
+      vim.keymap.set("n", "<space>fd", builtin.find_files)
+      
+      -- Special shortcut to find files in your Neovim config folder
+      vim.keymap.set("n", "<space>en", function()
+        builtin.find_files {
+          cwd = vim.fn.stdpath("config")
+        }
+      end)
+    end
+  }
 }
