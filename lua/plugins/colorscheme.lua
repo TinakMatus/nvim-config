c97d053151df9e997f19c9d0ca9a74790a00326f
tinakmatus <matustinak1@gmail.com>
2025-12-30 22:02:14 +0100

    made my pywal a bit better

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,13 +6,23 @@ return {
     config = function()
       local pywal = require("pywal")
       pywal.setup({
-        -- This ensures Neovim NEVER paints a background color
+
         transparent_background = true, 
       })
       vim.cmd.colorscheme "pywal"
-      
-      -- FORCE transparency for everything (borders, floats, etc.)
-      local groups = { "Normal", "NormalFloat", "FloatBorder", "TelescopeNormal", "TelescopeBorder", "NvimTreeNormal" }
+
+      local groups = {
+        "Normal",
+        "NormalFloat",
+        "FloatBorder",
+        "TelescopeNormal",
+        "TelescopeBorder",
+        "NvimTreeNormal",
+        "LineNr",
+        "CursorLineNr",
+        "SignColumn"
+      }
+
       for _, group in ipairs(groups) do
         vim.api.nvim_set_hl(0, group, { bg = "none", ctermbg = "none" })
       end
